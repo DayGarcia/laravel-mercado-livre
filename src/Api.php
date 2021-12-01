895696e0f72742425e82dc754955452dd3c85bb4
Dayanne Garcia <48609156+DayGarcia@users.noreply.github.com>
2021-12-01 16:16:34 -0300

    api default response changed to std class

--- src/Api.php
+++ src/Api.php
@@ -4,17 +4,18 @@ namespace DayGarcia\LaravelMercadoLivre;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Http;
+use stdClass;
 
 class Api
 {
     private const url = 'https://api.mercadolibre.com/';
 
-    public function get(String $access_token, String $url, array $params = []): JsonResponse
+    public function get(String $access_token, String $url, array $params = []): stdClass
     {
         return Http::withToken($access_token)->get(self::url . $url, $params)->object();
     }
 
-    public function post($url, $params = [])
+    public function post($url, $params = []): stdClass
     {
         return Http::post($url, $params)->object();
     }
