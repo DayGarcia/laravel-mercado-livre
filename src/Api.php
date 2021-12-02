c3b1e631d8594c84ce5482d49df2af372fd8df0e
Dayanne Garcia <48609156+DayGarcia@users.noreply.github.com>
2021-12-02 15:58:02 -0300

    file upload implemented

--- src/Api.php
+++ src/Api.php
@@ -19,4 +19,22 @@ class Api
     {
         return Http::withToken($access_token)->post(self::url . $url, $data)->object();
     }
+
+    public function put(string $access_token, $url, $data)
+    {
+        return Http::withToken($access_token)->put(self::url . $url, $data)->object();
+    }
+
+    public function delete(string $access_token, $url)
+    {
+        return Http::withToken($access_token)->delete(self::url . $url)->object();
+    }
+
+    public function upload(string $access_token, $url, $file)
+    {
+        return Http::withToken($access_token)->attach(
+            'attachment',
+            file_get_contents($file),
+        )->post(self::url . $url)->object();
+    }
 }
