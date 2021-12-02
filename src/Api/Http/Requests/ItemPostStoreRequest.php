99cff469ae24036f67cfafc24cd80ead705915b0
Dayanne Garcia <48609156+DayGarcia@users.noreply.github.com>
2021-12-02 13:13:17 -0300

    added request files

---
+++ src/Api/Http/Requests/ItemPostStoreRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\LaravelMercadoLivre;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ItemPostStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'                     => 'required|string',
+            'category_id'               => 'required|integer',
+            'price'                     => 'required|numeric',
+            'currency_id'               => 'required|string',
+            'available_quantity'        => 'required|integer',
+            'buying_mode'               => 'required|string',
+            'listing_type_id'           => 'required|string',
+            'condition'                 => 'required|string',
+            'sale_terms'                => 'required|array:id,value_name',
+            'sale_terms.*.id'           => 'required|string',
+            'sale_terms.*.value_name'   => 'required|string',
+            'pictures'                  => 'required|array:source',
+            'pictures.*.source'         => 'required|string',
+            'attributes'                => 'required|array:id,value_name',
+            'attributes.*.id'           => 'required|string',
+            'attributes.*.value_name'   => 'required|string',
+        ];
+    }
+}
