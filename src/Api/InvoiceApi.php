a2ff87e630321aaee204508c65c79a1b793da9f5
Dayanne Garcia <48609156+DayGarcia@users.noreply.github.com>
2021-12-03 13:24:17 -0300

    implemented all get invoice routes

--- src/Api/InvoiceApi.php
+++ src/Api/InvoiceApi.php
@@ -23,4 +23,23 @@ class InvoiceApi extends Api
         $url = "users/{$user_id}/invoices/{$invoice_id}";
         return $this->get($this->configuration->getAccessToken(), $url);
     }
+
+    public function getOrderInvoice(string $user_id, string $invoice_id)
+    {
+        $url = "users/{$user_id}/invoices/{$invoice_id}";
+        return $this->get($this->configuration->getAccessToken(), $url);
+    }
+
+    public function getShipmentInvoice(string $user_id, string $shipment_id)
+    {
+        $url = "users/{$user_id}/invoices/orders/shipments/{$shipment_id}";
+        return $this->get($this->configuration->getAccessToken(), $url);
+    }
+
+    // not tested yet
+    public function getBatchInvoice(string $user_id, string $site_id, string $period)
+    {
+        $url = "users/{$user_id}/invoices/sites/{$site_id}/batch_request/period/{$period}";
+        return $this->get($this->configuration->getAccessToken(), $url);
+    }
 }
