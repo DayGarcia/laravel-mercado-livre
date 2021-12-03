d73de083368e3830faceb6d3634c8171824c34d4
Dayanne Garcia <48609156+DayGarcia@users.noreply.github.com>
2021-12-03 13:01:19 -0300

    get invoices added

---
+++ src/Api/InvoiceApi.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DayGarcia\LaravelMercadoLivre\Api;
+
+use DayGarcia\LaravelMercadoLivre\Api;
+use DayGarcia\LaravelMercadoLivre\Configuration;
+
+class CategoryApi extends Api
+{
+    public function __construct(Configuration $configuration)
+    {
+        $this->configuration = $configuration;
+    }
+
+    public function getInvoices(string $user_id)
+    {
+        $url = "users/{$user_id}/invoices/orders";
+        return $this->get($this->configuration->getAccessToken(), $url);
+    }
+
+    public function getInvoice(string $user_id, string $invoice_id)
+    {
+        $url = "users/{$user_id}/invoices/{$invoice_id}";
+        return $this->get($this->configuration->getAccessToken(), $url);
+    }
+}
